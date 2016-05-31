e9e3ea64e4cdab92adf9068b1962838f7ec07aba
bemkap <bemkap@gmail.com>
2016-05-31 14:14:11 -0300

    lua cc hh

---
+++ lua.cc
@@ -0,0 +1,16 @@
+#include"instance.hh"
+#include"lua.hh"
+/*
+int translate(lua_State*L){
+  instance*i=(instance*)lua_touserdata(L,1);
+  double dx=lua_tonumber(L,2);
+  double dy=lua_tonumber(L,3);
+  i->x+=dx;
+  i->y+=dy;
+}
+int rotate(lua_State*L){
+  instance*i=(instance*)lua_touserdata(L,1);
+  double da=lua_tonumber(L,2);
+  
+}
+*/
---
+++ lua.hh
@@ -0,0 +1,9 @@
+#pragma once
+/*
+int x(lua_State*);
+int y(lua_State*);
+int rotate(lua_State*);
+int set(lua_State*);
+int instancecreate(lua_State*);
+int instancedestroy(lua_State*);
+*/
